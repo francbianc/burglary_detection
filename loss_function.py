429097d373fbba3bbb0fc9352cc98ee95de36c10
Davide Castellini <davide.castellini@studbocconi.it>
2020-11-30 14:59:36 +0100

    commented loss_function.py

--- loss_function.py
+++ loss_function.py
@@ -1,6 +1,23 @@
 import tensorflow as tf
 
 def custom_objective(y_true, y_pred):
+    """
+    Implementation of Sultani et al. (2019) custom loss function for a MIL ranking model
+    
+    Parameters
+    ----------
+    y_true : tf.tensor
+        True label of the temporal segment in the positive/negative bag
+    y_pred : tf.tensor
+        Predicted label of the temporal segment in the positive/negative bag
+
+    Returns
+    ----------
+    float
+        Value of the loss function computed from the minibatch of 60 videos
+    """
+
+
     y_true = tf.reshape(y_true, [-1]) 
     y_pred = tf.reshape(y_pred, [-1]) 
 
@@ -18,10 +35,12 @@ def custom_objective(y_true, y_pred):
         mm = y_true[ii * n_seg:ii * n_seg + n_seg]
         sub_sum_labels = tf.concat([sub_sum_labels, [tf.math.reduce_sum(mm)]], 0)   
 
+        # Smoothing constraint
         Feat_Score = y_pred[ii * n_seg:ii * n_seg + n_seg]
         sub_max = tf.concat([sub_max, [tf.reduce_max(Feat_Score, 0)]], 0)           
         sub_sum_l1 = tf.concat([sub_sum_l1, [tf.math.reduce_sum(Feat_Score)]], 0)   
 
+        # Sparsity constraint
         z1 = tf.ones_like(Feat_Score)
         z2 = tf.concat([z1, Feat_Score], 0)
         z3 = tf.concat([Feat_Score, z1], 0)
@@ -35,6 +54,7 @@ def custom_objective(y_true, y_pred):
     sub_score = sub_max[Num_d:]
     F_labels = sub_sum_labels[Num_d:]
 
+    # Smoothing and sparsity constraints
     sub_sum_l1 = sub_sum_l1[Num_d:]
     sub_sum_l1 = sub_sum_l1[:n_exp]
     sub_l2 = sub_l2[Num_d:]
@@ -45,6 +65,7 @@ def custom_objective(y_true, y_pred):
 
     n_Nor = n_exp
 
+    # Hinge loss
     Sub_Nor = tf.gather_nd(sub_score, indx_nor)                                     
     Sub_Abn = tf.gather_nd(sub_score, indx_abn)                                     
     z = tf.ones_like(y_true)
@@ -53,7 +74,10 @@ def custom_objective(y_true, y_pred):
         sub_z = tf.reduce_max(1 - Sub_Abn + Sub_Nor[ii], 0)
         z = tf.concat([z, [tf.math.reduce_sum(sub_z)]], 0)                            
 
+    # Complete loss function
+    lambda_1 = 0.00008
+    lambda_2 = 0.00008
     z = z[Num_d:]
-    z = tf.math.reduce_mean(z) + 0.00008 * tf.math.reduce_sum(sub_sum_l1) + 0.00008 * tf.math.reduce_sum(sub_l2)
+    z = tf.math.reduce_mean(z) + lambda_1 * tf.math.reduce_sum(sub_sum_l1) + lambda_2 * tf.math.reduce_sum(sub_l2)
     
     return z
