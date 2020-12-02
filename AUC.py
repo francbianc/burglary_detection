dc498493d1fedbefcf79b817ed30af6b553dc512
Francesca Bianchessi <62222730+francbianc@users.noreply.github.com>
2020-12-03 00:37:32 +0100

    Repo Version 4.0

--- AUC.py
+++ AUC.py
@@ -4,17 +4,22 @@ from utils.visualization_util import *
 from sklearn import metrics
 import configuration as cfg
 
+## USE THIS EXTENSION IF YOU'VE USED ONLY FEW VIDEOS SO FAR
+
 # PATHS: cfg.all_ann_path, cfg.input_folder, cfg.score_path, cfg.C3D_path, cfg.I3D_path
 # VARIABLES: cfg.use_i3d
+# AIM: compute AUC for test data 
 
 no_video = 1
 frm_counter = 0
 All_Detect = np.zeros(1000000)
 All_GT = np.zeros(1000000)
 
+# Get the true scores for each test video
 all_annotations = [line.strip().split() for line in open(cfg.all_ann_path, 'r')]
 all_annotations_dict = {l[0].split('.')[0]:l[-4:] for l in all_annotations}
 
+# Get the number of frames for each test video
 all_frames = [i.strip().split() for i in open('./num_frames.txt', 'r')]
 all_frames_dict = {l[0].split('/')[1]:l[1:] for l in all_frames}
 
@@ -44,9 +49,9 @@ for filename in os.listdir(cfg.input_folder):
       num_frames = int(all_frames_dict[name_txt][0])
       # integer
       
-      # assign to each frame the anomaly score of the feature it belongs to
-      num_features = len(C3D_file)     # must be 32
-      num_frames_C3D = num_features*16 # as the features were computed for every 16 frames
+      # Assign to each frame the anomaly score of the feature it belongs to
+      num_features = int(np.round(num_frames/16))
+      num_frames_C3D = num_features*16 # As the features were computed for every 16 frames
       Detection_score_32shots = np.zeros(num_frames_C3D)
       Thirty2_shots = np.round(np.linspace(0, num_features, 32))
 
@@ -71,6 +76,7 @@ for filename in os.listdir(cfg.input_folder):
 
       #print(num_frames)
       #print(len(Detection_score_32shots))
+      # Assign to the last frames of a video the 32th score 
       if num_frames > len(Detection_score_32shots):
          Final_score = np.append(Detection_score_32shots, np.repeat(Detection_score_32shots[-1], [num_frames-len(Detection_score_32shots)]))
          GT=np.zeros(num_frames)
@@ -78,7 +84,7 @@ for filename in os.listdir(cfg.input_folder):
          Final_score = Detection_score_32shots
          GT=np.zeros(len(Detection_score_32shots))
 
-      # check the temporal annotation
+      # Check the temporal annotation
       t_txt = [int(i) for i in Ann]
       
       for y in range(0,3,2):
@@ -93,6 +99,7 @@ for filename in os.listdir(cfg.input_folder):
       no_video = no_video + 1
       frm_counter=frm_counter+len(Final_score)
 
+# Compute the AUC according to Sultani et al. (2019)
 All_Detect = All_Detect[0:frm_counter]
 All_GT = All_GT[0:frm_counter]
 tot_scores = All_Detect
@@ -108,6 +115,7 @@ prec = tp / (fp + tp)
 AUC1 = np.trapz(rec, fpr)
 print('AUC1: ', AUC1)
 
+# Compute the AUC with the Scikit-Learn function
 fpr, tpr, thresholds = metrics.roc_curve(All_GT, All_Detect)
 AUC2 = metrics.auc(fpr, tpr)
 print('AUC2: ', AUC2)
