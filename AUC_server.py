9f37a362301bc64715e5c2ef80f7d93b4fd55c08
Francesca Bianchessi <62222730+francbianc@users.noreply.github.com>
2020-12-03 16:16:53 +0100

    Repo Version 4.0

--- AUC_server.py
+++ AUC_server.py
@@ -46,12 +46,14 @@ gio_frames_dict = {k:v for k,v in all_frames_dict.items() if k.split('/')[1].rep
 assert len(gio_frames_dict ) == 20
 sul_frames_dict = {k:v for k,v in all_frames_dict.items() if k.split('/')[1].replace('.txt', '') not in gios}
 
+# Consider only normal videos in the test set (normal means no anomaly at all)
+sul_normal_ann_names = [i for i in all_ann_names if i.split('/')[1].startswith('Normal')]
+sul_normal_frames_dict = {k:v for k,v in all_frames_dict.items() if k.split('/')[1].startswith('Normal')}
+
 def run_test(all_annotations_dict, all_frames_dict, all_ann_names):
    """
    Using the temporal annotations (true labels) that indicates the presence of an anomaly in the video and the frame predictions obtained with
-   the classifier, compute the accuracy of the model using the AUC using two different methods:
-   1. Sultani et al. (2019) approach
-   2. Scikit-Learn function
+   the classifier, compute the accuracy of the model using the AUC using scikit-learn.
    
    Parameters
    --------------
@@ -65,10 +67,7 @@ def run_test(all_annotations_dict, all_frames_dict, all_ann_names):
    Returns
    --------------
    float
-      AUC computed using the approach of Sultani et al. (2019)
-   float
       AUC computed using the ROC curve from the Scikit-Learn library
-
    """
 
    no_video = 1
@@ -148,28 +147,96 @@ def run_test(all_annotations_dict, all_frames_dict, all_ann_names):
 
    All_Detect = (All_Detect[0:frm_counter])
    All_GT = All_GT[0:frm_counter]
-   tot_scores = All_Detect
-
-   si = tot_scores.argsort()[::-1]
-   tp = All_GT[si] > 0
-   fp = All_GT[si] == 0
-   tp = np.cumsum(tp)
-   fp = np.cumsum(fp)
-
-   nrpos = sum(All_GT)
-   rec = tp / nrpos
-   fpr = fp / sum(All_GT == 0)
-   prec = tp / (fp + tp)
-
-   AUC1 = np.trapz(rec, fpr)
-   print('AUC1: ', AUC1)
-
+   
    fpr, tpr, thresholds = metrics.roc_curve(All_GT, All_Detect)
-   AUC2 = metrics.auc(fpr, tpr)
-   print('AUC2: ', AUC2)
+   AUC = metrics.auc(fpr, tpr)
+   print('AUC: ', AUC)
+   return AUC
 
-   return AUC1, AUC2
 
+def false_alarms(sul_normal_ann_names, sul_normal_frames_dict):
+   """
+   Using only normal videos, compute the percentage of normal videos that have been wrongly predicted 
+   as abnormal. This percentage represents the False Positive predictions. The threshold is set at 50%. 
+   
+   Returns
+   --------------
+   float
+      False Positive ratio 
+   """
+   no_video = 1
+   frm_counter = 0
+   All_GT = np.zeros(100000000)
+
+   for filename in sul_normal_ann_names:
+      if filename.endswith('.txt'):
+         video_name = os.path.join(path_all_features, filename)
+         name = os.path.basename(video_name).split('.')[0]
+         #print(name)
+         name_txt = name + '.txt'
+         
+         scores = os.path.join(score_path, name_txt)
+         score = [line.strip() for line in open(scores, 'r')]
+         # list of 32 str (each str contains 1 score)
+
+         scores = os.path.join(score_path, name_txt)
+         score = [line.strip() for line in open(scores, 'r')]
+         # list of 32 str (each str contains 1 score) 
+
+         num_frames = int(sul_normal_frames_dict[filename][0])
+         # integer
+         
+         # assign to each frame the anomaly score of the feature it belongs to
+         #num_features = len(C3D_file)     
+         num_features = int(np.round(num_frames/16))
+         num_frames_C3D = num_features*16 # as the features were computed for every 16 frames
+         Detection_score_32shots = np.zeros(num_frames_C3D)
+         Thirty2_shots = np.round(np.linspace(0, num_features, 32))
+
+         l = range(len(Thirty2_shots))
+         p_c = -1
+         for c_shots, n_shots in zip (l, l[1:]):
+            p_c = p_c + 1
+            ss = Thirty2_shots[c_shots]
+            ee = Thirty2_shots[n_shots] - 1
+            #print('ss:', ss, 'ee:', ee)
+            #print('c_shots:', c_shots, 'n_shots:', n_shots)
+
+            if c_shots == len(Thirty2_shots):
+               ee=Thirty2_shots[n_shots]
+
+            if ee<ss:
+               Detection_score_32shots[(int(ss))*16:(int(ss))*16+16+1] = score[p_c]
+               #print(ee < ss)
+            else:
+               Detection_score_32shots[(int(ss))*16:(int(ee))*16+16+1] = score[p_c]
+               #print(ee > ss)
+
+         #print(num_frames)
+         #print(len(Detection_score_32shots))
+
+         if num_frames > len(Detection_score_32shots):
+            Final_score = np.append(Detection_score_32shots, np.repeat(Detection_score_32shots[-1], [num_frames-len(Detection_score_32shots)]))
+            GT=np.zeros(num_frames)
+         else:
+            Final_score = Detection_score_32shots
+            GT=np.zeros(len(Detection_score_32shots))
+         
+         for i in range(len(Final_score)):
+            # Set the threshold at 50%
+            if Final_score[i] > 0.5:
+                GT[i] = 1
+
+         All_GT[frm_counter:frm_counter+len(Final_score)] = GT
+         #print('Video ', no_video, ' successfully processed!')
+         no_video = no_video + 1
+         frm_counter = frm_counter+len(Final_score)
+
+   All_GT = All_GT[0:frm_counter]
+   no_false_alarms = All_GT.sum()
+   perc_false_alarms = no_false_alarms/frm_counter
+   print('perc_false_alarms: ', perc_false_alarms)
+   return perc_false_alarms
 
 # Compute the AUC considering all the test videos
 print('>> AUC with 310 test videos')
@@ -182,3 +249,7 @@ AUC1_sul, AUC2_sul = run_test(sul_annotations_dict, sul_frames_dict, sul_ann_nam
 # Compute the AUC considering only the test videos we've collected 
 print('>> AUC with 20 GIOSS test videos')
 AUC1_gio, AUC2_gio = run_test(gio_annotations_dict, gio_frames_dict, gio_ann_names)
+
+# Compute the FP ratio for Normal Videos 
+print('>> Percentage of False Alarms in NORMAL VIDEOS')
+perc_false_alarms = false_alarms(sul_normal_ann_names, sul_normal_frames_dict)
